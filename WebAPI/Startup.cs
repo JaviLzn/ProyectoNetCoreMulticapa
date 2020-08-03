a2da05a7a7ea5ebb12331c65ee111dc81bfe1c76
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-03 09:44:22 -0500

    Se cambiar versiones en los proyecto a NetCore3.1 Se deshabilita soporte HTTPS para env dev

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
