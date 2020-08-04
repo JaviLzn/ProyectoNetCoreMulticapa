cb5a2861642b286f127318e0e6e61f7d56b24b15
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-04 10:07:09 -0500

    Se agrega precision en los campos money de la entidad Precio

--- Persistencia/CursosOnlineContext.cs
+++ Persistencia/CursosOnlineContext.cs
@@ -13,6 +13,8 @@ namespace Persistencia
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.InstructorId, ci.CursoId });
+            modelBuilder.Entity<Precio>().Property(p => p.PrecioActual).HasColumnType("decimal(19,4)");
+            modelBuilder.Entity<Precio>().Property(p => p.Promocion).HasColumnType("decimal(19,4)");
         }
 
         public DbSet<Comentario> Comentario { get; set; }
