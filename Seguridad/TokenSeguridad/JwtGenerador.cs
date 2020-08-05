0deba7f8a1e7c65a58f12fb55b8000d9500e30ea
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-05 06:17:37 -0500

    Logica para generador de tokens

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Security.Claims;
+using System.Collections.Generic;
 using Aplicacion.Contratos;
 using Dominio;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Seguridad
 {
@@ -7,7 +13,25 @@ namespace Seguridad
     {
         public string CrearToken(Usuario usuario)
         {
-            throw new System.NotImplementedException();
+            // * Lista de Claims
+            // La data del usuario que quiero compartir
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            var credenciales  = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescipcion = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales
+            };
+
+            var tokenManejador = new JwtSecurityTokenHandler();
+            var token = tokenManejador.CreateToken(tokenDescipcion);
+
+            return tokenManejador.WriteToken(token);
         }
     }
 }
