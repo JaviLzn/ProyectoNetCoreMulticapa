9ba9f9390523f09614a0c4a19801c08ae1679c22
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-05 16:21:56 -0500

    Se crea logica para registrar nuevo usuario

---
+++ Aplicacion/Seguridad/Registrar.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.Contratos;
+using Aplicacion.ManejadorError;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+
+namespace Aplicacion.Seguridad
+{
+    public class Registrar
+    {
+        public class Ejecuta : IRequest<UsuarioData>
+        {
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string UserName { get; set; }
+        }
+
+        public class EjecutaValidador : AbstractValidator<Ejecuta>{
+
+            public EjecutaValidador()
+            {
+                RuleFor(x => x.Nombre).NotEmpty();
+                RuleFor(x => x.Apellidos).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty().EmailAddress();
+                RuleFor(x => x.Password).NotEmpty();
+                RuleFor(x => x.UserName).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, UsuarioData>
+        {
+            private readonly CursosOnlineContext _context;
+            private readonly UserManager<Usuario> _userManager;
+            private readonly IJwtGenerador _jwtGenerador;
+            public Manejador(CursosOnlineContext context, UserManager<Usuario> userManager, IJwtGenerador jwtGenerador)
+            {
+                _context = context;
+                _userManager = userManager;
+                _jwtGenerador = jwtGenerador;
+            }
+
+            public async Task<UsuarioData> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var existeEmail = await _context.Users.Where(x => x.Email == request.Email).AnyAsync();
+                if (existeEmail)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.BadRequest, new { mensaje = "El Email se encuentra en uso."});
+                };
+
+                var existeUserName = await _context.Users.Where(x => x.UserName == request.UserName).AnyAsync();
+                if (existeUserName)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.BadRequest, new { mensaje = "El Nombre de Usuario ya se encuentra en uso."});
+                };
+
+                var usuario = new Usuario {
+                    NombreCompleto = request.Nombre + " " + request.Apellidos,
+                    Email = request.Email,
+                    UserName = request.UserName
+                };
+
+                var resultado = await _userManager.CreateAsync(usuario, request.Password);
+
+                if (resultado.Succeeded)
+                {
+                   return new UsuarioData {
+                       NombreCompleto = usuario.NombreCompleto,
+                       Token = _jwtGenerador.CrearToken(usuario),
+                       UserName = usuario.UserName
+                   };
+                }
+
+                throw new Exception("No se pudo agregar el nuevo usuario");
+            }
+        }
+
+
+    }
+}
