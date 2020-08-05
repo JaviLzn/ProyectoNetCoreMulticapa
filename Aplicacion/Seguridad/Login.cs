99b3a38a635ffe69ceef188207c61ee492868649
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-04 20:46:16 -0500

    Logica para Login

---
+++ Aplicacion/Seguridad/Login.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.ManejadorError;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Aplicacion.Seguridad
+{
+    public class Login
+    {
+        public class Ejecuta : IRequest<Usuario>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class EjecutaValidacion : AbstractValidator<Ejecuta> {
+            public EjecutaValidacion(){
+                RuleFor( x => x.Email).NotEmpty();
+                RuleFor( x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, Usuario>
+        {
+            private readonly UserManager<Usuario> _userManager;
+            private readonly SignInManager<Usuario> _signInManager;
+            public Manejador(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+            public async Task<Usuario> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var usuario = await _userManager.FindByEmailAsync(request.Email);
+                if (usuario == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.Unauthorized);
+                }
+                var resultado = await _signInManager.CheckPasswordSignInAsync(usuario, request.Password, false);
+
+                if (resultado.Succeeded)
+                {
+                    return usuario;
+                }
+
+                throw new ManejadorExcepcion(HttpStatusCode.Unauthorized);
+
+            }
+        }
+    }
+}
