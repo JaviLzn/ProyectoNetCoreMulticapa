5db8d6b840b4a37aafa8a7f6209296271da3abbe
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-08 10:05:29 -0500

    Lógica para borrar un comentario

---
+++ Aplicacion/Comentarios/Eliminar.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.ManejadorError;
+using MediatR;
+using Persistencia;
+
+namespace Aplicacion.Comentarios
+{
+    public class Eliminar
+    {
+        public class Ejecuta : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class ManejarEjecuta : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext context;
+
+            public ManejarEjecuta(CursosOnlineContext context)
+            {
+                this.context = context;
+            }
+
+            public async  Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var comentario = await context.Comentario.FindAsync(request.Id);
+                if (comentario == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje ="No se encotró el comentario a borrar"});
+                }
+
+                context.Comentario.Remove(comentario);
+
+                var resultado = await context.SaveChangesAsync();
+                if (resultado > 0 )
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo eliminar el comentario");
+            }
+        }
+    }
+}
