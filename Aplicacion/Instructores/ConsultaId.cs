d2fa35002832a45fc169510bd89fc03ca79417e8
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-08 07:56:49 -0500

    Se Implementa lógica para consultar un instructor por Id

---
+++ Aplicacion/Instructores/ConsultaId.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.ManejadorError;
+using MediatR;
+using Persistencia.DapperConexion.Instructor;
+
+namespace Aplicacion.Instructores
+{
+    public class ConsultaId
+    {
+        public class ObtenerInstructorPorId : IRequest<InstructorModel>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class ManejarObtenerInstructorPorId : IRequestHandler<ObtenerInstructorPorId, InstructorModel>
+        {
+            private readonly IInstructor instructorRepositorio;
+
+            public ManejarObtenerInstructorPorId(IInstructor instructorRepositorio)
+            {
+                this.instructorRepositorio = instructorRepositorio;
+            }
+
+            public async Task<InstructorModel> Handle(ObtenerInstructorPorId request, CancellationToken cancellationToken)
+            {
+                var instructor = await instructorRepositorio.ObtenerPorId(request.Id);
+
+                if (instructor == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje = "No se encontró el instructor"});
+                }
+
+                return instructor;
+            }
+        }
+    }
+}
