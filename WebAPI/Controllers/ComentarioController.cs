5cc1090d709785ac515396b9dd03946caf5f3411
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-08 10:07:29 -0500

    Nuevo EndPoint: Eliminar Comentario

--- WebAPI/Controllers/ComentarioController.cs
+++ WebAPI/Controllers/ComentarioController.cs
@@ -12,5 +12,10 @@ namespace WebAPI.Controllers
         public async Task<ActionResult<Unit>> CrearComentario (Nuevo.CrearComentario data){
             return await Mediator.Send(data);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> EliminarComentario (Guid id){
+            return await Mediator.Send(new Eliminar.Ejecuta{Id = id});
+        }
     }
 }
