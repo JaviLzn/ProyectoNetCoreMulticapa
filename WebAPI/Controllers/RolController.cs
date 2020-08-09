9c2741c795b43f92db2248c4824750d72482e57a
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-08 20:18:04 -0500

    Se crea endpoint: para listar roles

--- WebAPI/Controllers/RolController.cs
+++ WebAPI/Controllers/RolController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Aplicacion.Seguridad;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -15,6 +17,11 @@ namespace WebAPI.Controllers
         [HttpDelete("eliminar")]
         public async Task<ActionResult<Unit>> EliminarRol (RolEliminar.Ejecuta data){
             return await Mediator.Send(data);
+        }
+
+        [HttpGet("lista")]
+        public async Task<ActionResult<List<IdentityRole>>> ListaRoles(){
+            return await Mediator.Send(new RolLista.Ejecuta());
         }
     }
 }
