40a1ed2e57c1570ab77200dc2060c36cfd23df7a
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-08 20:17:34 -0500

    Se implementa la lista de Roles

---
+++ Aplicacion/Seguridad/RolLista.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplicacion.Seguridad
+{
+    public class RolLista
+    {
+         public class Ejecuta: IRequest<List<IdentityRole>>{}
+
+        public class ManejarEjecuta : IRequestHandler<Ejecuta, List<IdentityRole>>
+        {
+            private readonly RoleManager<IdentityRole> roleManager;
+
+            public ManejarEjecuta(RoleManager<IdentityRole> roleManager)
+            {
+                this.roleManager = roleManager;
+            }
+
+            public async Task<List<IdentityRole>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                return await roleManager.Roles.ToListAsync();
+            }
+        }
+    }
+}
