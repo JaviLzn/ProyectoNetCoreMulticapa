5ab54cdaf3c216d6d21c81ab46ccb70d58cda43b
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-10 11:00:52 -0500

    Lógica para obtener los roles de un usuario

---
+++ Aplicacion/Seguridad/ObtenerRolesUsuario.cs
@@ -0,0 +1,56 @@
+﻿using Aplicacion.ManejadorError;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Seguridad
+{
+    public class ObtenerRolesUsuario
+    {
+        public class Ejecuta : IRequest<List<string>>
+        {
+            public string UserName { get; set; }
+        }
+
+
+        public class ValidadorEjecuta : AbstractValidator<Ejecuta>
+        {
+            public ValidadorEjecuta()
+            {
+                RuleFor(x => x.UserName).NotEmpty();
+            }
+        }
+
+        public class ManejarEjecuta : IRequestHandler<Ejecuta, List<string>>
+        {
+            private readonly UserManager<Usuario> userManager;
+            private readonly RoleManager<IdentityRole> roleManager;
+
+            public ManejarEjecuta(UserManager<Usuario> userManager, RoleManager<IdentityRole> roleManager)
+            {
+                this.userManager = userManager;
+                this.roleManager = roleManager;
+            }
+
+            public async Task<List<string>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var usuario = await userManager.FindByNameAsync(request.UserName);
+                if (usuario == null)
+                {
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje = "No existe el usuario" });
+                }
+
+                var resultado = await userManager.GetRolesAsync(usuario);
+                return new List<string>(resultado);
+
+            }
+        }
+    }
+}
