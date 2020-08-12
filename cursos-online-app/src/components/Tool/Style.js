3e1551adadd169bbeb9c72fba5fa94d3bf05ea10
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-12 18:48:21 -0500

    Nuevo component para estilos

---
+++ cursos-online-app/src/components/Tool/Style.js
@@ -0,0 +1,33 @@
+const style = {
+    paper: {
+        marginTop: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100%',
+    },
+    form: {
+        width: '100%',
+        marginTop: 20,
+    },
+    submit: {
+        marginTop: 15,
+    },
+    containerCenterXY: {
+        position: 'absolute',
+        left: '50%',
+        top: '40%',
+        transform: 'translate(-50%, -50%)',
+    },
+    avatar: {
+        margin: 5,
+        backgroundColor: '#1976d2',
+        width: 60,
+        height: 60
+    },
+    icon: {
+        fontSize: 40
+    }
+};
+
+export default style;
