de51fc2f81608f88416a8e721bc6ca74b1f00ead
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-12 07:08:58 -0500

    Se crea theme, se configura y se implementa

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './theme';
+import { TextField, Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Bienvenidos al curso de Sindry</h1>
-    </div>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <h1>Proyecto en blanco</h1>
+            <TextField variant="outlined" />
+            <Button variant="contained" color="primary">Mi boton Material Design</Button>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
---
+++ cursos-online-app/src/theme/palette.js
@@ -0,0 +1,10 @@
+const palette = {
+    primary: {
+        light: '#63a4fff',
+        main: '#1976d2',
+        dark: '#004ba0',
+        contrastText: '#ecfad8',
+    },
+};
+
+export default palette;
