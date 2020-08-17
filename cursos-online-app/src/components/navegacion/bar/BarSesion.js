ba7ef9aee2c81715467a3c379c36ce8c8486cae0
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-17 14:45:44 -0500

    Se cambia la ruta del makestyles al importar Se desistaló dependencia que no se necesitaba

--- cursos-online-app/src/components/navegacion/bar/BarSesion.js
+++ cursos-online-app/src/components/navegacion/bar/BarSesion.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Toolbar, IconButton, Typography, Button, Avatar } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     seccionDesktop: {
