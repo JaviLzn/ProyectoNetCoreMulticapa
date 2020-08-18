25c80b8471d7dec23c7b32e8b21d2e694fa1dbf5
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-18 14:02:09 -0500

    se obtiene el usuario actual al cargar la app

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import Login from './components/seguridad/Login';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import RegistrarUsuario from './components/seguridad/RegistrarUsuario';
 import PerfilUsuario from './components/seguridad/PerfilUsuario';
 import AppNavbar from './components/navegacion/AppNavbar';
+import { useStateValue } from './context/store';
+import { obtenerUsuarioActual } from './actions/UsuarioAction';
 
 function App() {
+    const [, dispatch] = useStateValue();
+
+    useEffect(() => {
+        obtenerUsuarioActual(dispatch);
+    },[]);
+
     return (
         <Router>
             <ThemeProvider theme={theme}>
