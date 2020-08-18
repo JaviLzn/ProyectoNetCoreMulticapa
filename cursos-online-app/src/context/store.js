ffa239a69135ff7f6a53a92d7c5f127211a9d1cc
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-18 14:00:08 -0500

    Se corrige bug en el store

--- cursos-online-app/src/context/store.js
+++ cursos-online-app/src/context/store.js
@@ -2,10 +2,10 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 export const StateContext = createContext();
 
-export const StateProvider = ({reducer, initalState, children}) =>(
-    <StateProvider value = {useReducer(reducer, initalState)}>
-       {children} 
-    </StateProvider>
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
 );
 
 export const useStateValue = () => useContext(StateContext);
