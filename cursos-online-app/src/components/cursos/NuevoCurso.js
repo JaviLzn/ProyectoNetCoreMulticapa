c5ab47016af82d7b203726be8af04eb7af0d51b2
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-24 19:35:04 -0500

    Se agregan dependencias: @material-ui/pickers @date-io/date-fns date-fns

--- cursos-online-app/src/components/cursos/NuevoCurso.js
+++ cursos-online-app/src/components/cursos/NuevoCurso.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../Tool/Style';
 import { Typography, Container, Grid, TextField, Button } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import esLocale from 'date-fns/locale/es';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 
 const NuevoCurso = () => {
+    const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
+
+    const [curso, setCurso] = useState({
+        Titulo: '',
+        Descripcion: '',
+        Precio: 0.0,
+        Promocion: 0.0,
+    });
+
+    const cargarValoresCurso = (e) => {
+        const { name, value } = e.target;
+        setCurso((anterior) => ({ ...anterior, [name]: value }));
+    };
+
     return (
         <Container component='main' maxWidth='xs' justify='center'>
             <div style={style.paper}>
@@ -12,16 +29,32 @@ const NuevoCurso = () => {
                 <form style={style.form}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <TextField name='Titulo' variant='outlined' fullWidth label='Ingrese Titulo' />
+                            <TextField name='Titulo' value={curso.Titulo} onChange={cargarValoresCurso} variant='outlined' fullWidth label='Ingrese Titulo' />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField name='Descripcion' variant='outlined' fullWidth label='Ingrese Descripcion' />
+                            <TextField name='Descripcion' value={curso.Descripcion} onChange={cargarValoresCurso} variant='outlined' fullWidth label='Ingrese Descripcion' />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField name='Precio' variant='outlined' fullWidth label='Ingrese Precio Normal' />
+                            <TextField name='Precio' value={curso.Precio} onChange={cargarValoresCurso} variant='outlined' fullWidth label='Ingrese Precio Normal' />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField name='Promocion' variant='outlined' fullWidth label='Ingrese Precio de promoción' />
+                            <TextField name='Promocion' value={curso.Promocion} onChange={cargarValoresCurso} variant='outlined' fullWidth label='Ingrese Precio de promoción' />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
+                                <KeyboardDatePicker
+                                    value={fechaSeleccionada}
+                                    onChange={setFechaSeleccionada}
+                                    margin='normal'
+                                    id='fecha-publicacion-id'
+                                    label='Seleccione fecha de publicacion'
+                                    format='dd/MM/yyyy'
+                                    fullWidth
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                            </MuiPickersUtilsProvider>
                         </Grid>
                     </Grid>
                     <Grid container justify='center'>
