7dc77e96dfd0c97265dd86fdbcd8af21448b9f98
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-25 20:56:59 -0500

    Se crea controller para exportar Documentos

---
+++ WebAPI/Controllers/ExportarDocumentoController.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System.Threading.Tasks;
+using Aplicacion.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [AllowAnonymous]
+    public class ExportarDocumentoController : MiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Stream>> GetTask(){
+            return await Mediator.Send(new ExportPDF.Consulta());
+        }
+    }
+}
